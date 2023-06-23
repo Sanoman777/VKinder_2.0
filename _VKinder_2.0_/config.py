812c49b6a12917f0e012303b152d6ab7c2efea4e
Александр <sanoman8386@gmail.com>
2023-06-23 11:46:36 +0300

    recent changes

---
+++ _VKinder_2.0_/config.py
@@ -0,0 +1,9 @@
+comunity_token = ''
+
+acces_token = ''
+
+db_url_object = "postgresql://postgres:<password>@localhost:5432/datastore3"
+# db_url_object = "postgresql://<пользователь>:<пароль>@<хост>:<порт>/<имя_базы_данных>"
+
+# psql -h localhost -p 5432 -U postgres -d datastore3
+
